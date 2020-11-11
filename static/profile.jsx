3004cf8bd37dac139a50ba0e10a0af68f0680bde
vagrant <vagrant@vagrant.vm>
2020-11-11 22:46:51 +0000

    Cleaned up, organized and commented code in profile component.

--- static/profile.jsx
+++ static/profile.jsx
@@ -1,6 +1,10 @@
 "use strict";
 
+
+/* ============ Setup for User plant & Logic for deleting a plant from User's profile ============== */
+
 const UserPlant = (props) => {
+
     return (
       <Row className="userPlant">
         <p> {props.water_tip} </p>
@@ -11,37 +15,10 @@ const UserPlant = (props) => {
     );
 }
 
-const WishlistPlant = (props) => {
-  return (
-    <Row className="wishlistPlant">
-      <p> {props.water_tip} </p>
-      <p> {props.plant_name} </p>
-      <Image id="user-plant-photo" src={props.plant_image} rounded fluid />
-      <Row><Button variant="outline-secondary" onClick={(e) => removePlantFromWishlist(e)} id={props.plant_id}>Remove from wishlist</Button></Row>
-    </Row>
-  );
-}
-
-const removePlantFromWishlist= (e) => {
-  const plant_id = e.target.id;
-  const user_id = localStorage['user_id']
-
-  if(plant_id && user_id) {
-    fetch(`/delete_plant_from_wishlist/${plant_id}/${user_id}`, {
-        method: 'DELETE',
-        headers: {'Content-Type': 'application/json'}
-    })
-    .then(() => document.location.reload())
-    .catch((error) => console.log('Error in removing plant from profile.', error))
-  } else {
-    console.log('Missing plant_id', plant_id);
-  }
-}
-
-
 const removePlantFromProfile = (e) => {
+
   const plant_id = e.target.id;
-  console.log('plant id trying to delete:', plant_id)
+
   if(plant_id) {
     fetch(`/delete_plant_from_profile/${plant_id}`, {
         method: 'DELETE',
@@ -54,7 +31,38 @@ const removePlantFromProfile = (e) => {
   }
 }
 
-//'/delete_plant_from_wishlist/<plant_id>/<user_id>'
+/* ============ Setup for plants in User Wishlist & Logic for deleting a plant from User's Wishlist ============== */
+
+const WishlistPlant = (props) => {
+
+  return (
+    <Row className="wishlistPlant">
+      <p> {props.water_tip} </p>
+      <p> {props.plant_name} </p>
+      <Image id="user-plant-photo" src={props.plant_image} rounded fluid />
+      <Row><Button variant="outline-secondary" onClick={(e) => removePlantFromWishlist(e)} id={props.plant_id}>Remove from wishlist</Button></Row>
+    </Row>
+  );
+}
+
+const removePlantFromWishlist= (e) => {
+
+  const plant_id = e.target.id;
+  const user_id = localStorage['user_id'];
+
+  if(plant_id && user_id) {
+    fetch(`/delete_plant_from_wishlist/${plant_id}/${user_id}`, {
+      method: 'DELETE',
+      headers: {'Content-Type': 'application/json'}
+    })
+    .then(() => document.location.reload())
+    .catch((error) => console.log('Error in removing plant from profile.', error))
+  } else {
+    console.log('Missing plant_id', plant_id);
+  }
+}
+
+/*================= Search bar for searching for plants on a User's profile ====================*/
 
 const ProfilePlantsSearch = () => {
   return (
@@ -71,108 +79,102 @@ const ProfilePlantsSearch = () => {
   )
 }
 
+/*============= Container for Profile Page & Logic for fetching User plants and Wishlist ================*/
+
 const UserPlantsContainer = () => {
 
-      React.useEffect(() => {
-        const loggedInUserID = localStorage['user_id']
-        if(loggedInUserID) {
+  React.useEffect(() => {
 
-        console.log('user logged in...fetching user plants with: ', loggedInUserID)
-        fetch(`/get_user_plants.json/${loggedInUserID}`)
-        .then((response) => response.json())
-        .then((data) => {
-          updateUserPlants(data)
-        })
-        .catch((error) => updateUserPlants([]))
+    const loggedInUserID = localStorage['user_id'];
+
+    if(loggedInUserID) {
+      //Send a request for User's plants
+      fetch(`/get_user_plants.json/${loggedInUserID}`)
+      .then((response) => response.json())
+      .then((data) => {
+        updateUserPlants(data)
+      })
+      .catch(() => updateUserPlants([]))
+
+      //Send a request for User's wishlist
+      fetch(`/get_user_wishlist.json/${loggedInUserID}`)
+      .then((response) => response.json())
+      .then((wishlist) => updateWishlist(wishlist))
+      .catch(() => updateWishlist([]))
+    }
+  }, []);
 
 
-        console.log('fetching users wishlist...')
-        fetch(`/get_user_wishlist.json/${loggedInUserID}`)
-        .then((response) => response.json())
-        .then((wishlist) => updateWishlist(wishlist))
-        .catch((error) => updateWishlist([]))
+  //State for User's plants
+  const [userPlants, updateUserPlants] = React.useState('userPlants');
+  const userPlantsArr = [];
+  //State for User's wishlist
+  const [wishlist, updateWishlist] = React.useState('wishList');
+  const wishlistArr = [];
 
-        }
-      }, []);
-
-
-    const [userPlants, updateUserPlants] = React.useState('userPlants');
-    const userPlantsArr = [];
-    const [wishlist, updateWishlist] = React.useState('wishList');
-    const wishlistArr = [];
-
-    if(userPlants.length !== 0) {
-      for (let plant of userPlants) {
-        userPlantsArr.push(
-          <UserPlant
+  //If there are plants in the User plants array, create nodes for them to display on profile.
+  if(userPlants.length !== 0) {
+    for (let plant of userPlants) {
+      userPlantsArr.push(
+        <UserPlant
           plant_name={plant.plant_name}
           plant_image={plant.plant_image}
           water_tip={plant.water_tip}
           user_plant_id={plant.user_plant_id}
-          />
-        );
-      }
+        />
+      );
     }
+  }
 
-    if(wishlist.length !== 0) {
-      for (var i = 0; i < wishlist.length; i++) {
-        wishlistArr.push(
-            <WishlistPlant
-              plant_name={wishlist[i].plant_name}
-              plant_tip={wishlist[i].plant_tip}
-              plant_image={wishlist[i].plant_image}
-              plant_id={String(wishlist[i].plant_id)}
-            />
-        )
-      }
+  //If there are plants in the User wishlist array, create nodes for them to display on profile.
+  if(wishlist.length !== 0) {
+    for (let plant of wishlistArr) {
+      wishlistArr.push(
+        <WishlistPlant
+          plant_name={plant[i].plant_name}
+          plant_tip={plant[i].plant_tip}
+          plant_image={plant[i].plant_image}
+          plant_id={String(plant[i].plant_id)}
+        />
+      );
     }
- 
+  }
 
-
-
-const UserProfileInfo = () => {
-  // React.useEffect(() => {
-    //fetch for user info
-    //then parse info
-    //then update state with info
-  // })
-
-  // const [userInfo, updateUserInfo] = React.useState('userInfo');
-
-  return (
-    <Container>
-      <Row><h3>Plant Profile</h3></Row>
-      <Row>{userPlants.length} Plants</Row>
-    </Container>
-  )
-
-}
-
-const UserPlantWishlist = () => {
-
-  return (
-    <Container>
-      <Col>{wishlistArr}</Col>
-    </Container>
-  )
-}
-
-
+  //Contains plant count and nursery name
+  const UserProfileInfo = () => {
     return (
       <Container>
-
-        <Row>
-          <Col sm={4}><UserProfileInfo /></Col>
-          <Col sm={8}><ProfilePlantsSearch /></Col>
-        </Row>
-
-        <Row>
-          <Col sm={4}><UserPlantWishlist /></Col>
-          <Col sm={8}>{userPlantsArr}</Col>
-        </Row>
-
+        <Row><h3>Plant Profile</h3></Row>
+        <Row>{userPlants.length} Plants</Row>
       </Container>
-    );
+    )
+  }
+
+  //Contains a user's plant wishlist
+  const UserPlantWishlist = () => {
+    return (
+      <Container>
+        <Col>{wishlistArr}</Col>
+      </Container>
+    )
+  }
+
+
+  return (
+    <Container>
+
+      <Row>
+        <Col sm={4}><UserProfileInfo /></Col>
+        <Col sm={8}><ProfilePlantsSearch /></Col>
+      </Row>
+
+      <Row>
+        <Col sm={4}><UserPlantWishlist /></Col>
+        <Col sm={8}>{userPlantsArr}</Col>
+      </Row>
+
+    </Container>
+  );
 
 
 }
